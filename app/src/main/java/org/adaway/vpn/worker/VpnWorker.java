aaac3d87104bcb3e38027a4a40f23e1dd2f03947
PerfectSlayer <bruce.bujon@gmail.com>
2021-11-15 09:53:37 +0100

    feat(vpn): Improve log levels

--- app/src/main/java/org/adaway/vpn/worker/VpnWorker.java
+++ app/src/main/java/org/adaway/vpn/worker/VpnWorker.java
@@ -131,7 +131,7 @@ public class VpnWorker implements DnsPacketProxy.EventLoop {
      * Kill the current worker and restart it if already running.
      */
     public void start() {
-        Log.i(TAG, "Starting VPN thread…");
+        Log.d(TAG, "Starting VPN thread…");
         Thread workerThread = new Thread(this::work, "VpnWorker");
         setWorkerThread(workerThread);
         workerThread.start();
@@ -142,7 +142,7 @@ public class VpnWorker implements DnsPacketProxy.EventLoop {
      * Stop the VPN worker.
      */
     public void stop() {
-        Log.i(TAG, "Stopping VPN thread.");
+        Log.d(TAG, "Stopping VPN thread.");
         setWorkerThread(null);
         Log.i(TAG, "VPN thread stopped.");
     }
@@ -161,7 +161,7 @@ public class VpnWorker implements DnsPacketProxy.EventLoop {
     }
 
     private void work() {
-        Log.i(TAG, "Starting");
+        Log.d(TAG, "Starting work…");
         // Initialize context
         this.dnsPacketProxy.initialize(this.vpnService);
         // Initialize the watchdog
@@ -212,7 +212,7 @@ public class VpnWorker implements DnsPacketProxy.EventLoop {
         }
 
         this.vpnService.notifyVpnStatus(STOPPED);
-        Log.i(TAG, "Exiting");
+        Log.d(TAG, "Exiting work.");
     }
 
     private void runVpn() throws IOException, ErrnoException, VpnNetworkException {
@@ -416,7 +416,7 @@ public class VpnWorker implements DnsPacketProxy.EventLoop {
     }
 
     private ParcelFileDescriptor configure() throws VpnNetworkException {
-        Log.i(TAG, "Configuring" + this);
+        Log.d(TAG, "Configuring" + this);
 
         // TODO User configuration
 //        Configuration config = FileHelper.loadCurrentSettings(vpnService);
